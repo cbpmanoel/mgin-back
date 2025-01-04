3b5e35f6282c7ee5670ee9d38eb326c8400e991a
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-03 23:37:59 -0300

    Fix typo

--- src/models/menuitem.py
+++ src/models/menuitem.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, PositiveFloat
 
 class MenuItemModel(BaseModel):
     ''' Menu Item Model '''
-    cateory_id: int      = Field(description="Category ID")
+    category_id: int     = Field(description="Category ID")
     id: int              = Field(description="Menu Item ID")
     name: str            = Field(description="Menu Item Name")
     image_id: str        = Field(description="Image ID")
