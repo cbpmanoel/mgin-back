e929eeaf7abf3da9d4ed80c99df1f162d4a70b9e
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-04 18:32:04 -0300

    Enhance FastAPI setup with metadata and custom validation error handling

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from .src.routes.image import get_router as image_router
 from .src.routes.menu import get_router as menu_router
 from .src.routes.orders import get_router as orders_router
+from fastapi_metadata import metadata
 
 # FastAPI object
-app = FastAPI(title="MGin Kiosk API", description="API for MGin Kiosk", version="0.1", prefix="/api")
+app = FastAPI(prefix="/api/v1", **metadata)
+
+# Configure the exception handlers
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    ''' Handle validation errors as bad requests '''
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={'detail': 'Invalid request data', 'errors': exc.errors(), 'body': exc.body},
+    )
 
 # Add the endpoint routers
 app.add_route("/image", image_router())
---
+++ fastapi_metadata.py
@@ -0,0 +1,18 @@
+_documentation = """
+MGin Kiosk API to access the menu items and categories, images, and orders.
+"""
+
+metadata = {
+    "title": "MGinKioskAPI",
+    "description": "API for MGin Kiosk",
+    "version": "0.1.0",
+    "documentation": _documentation,
+    "contact": {
+        "name": "Manoel Pereira",
+        "url": "http://manoelpereira.com.br",
+    },
+    "license": {
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+}
