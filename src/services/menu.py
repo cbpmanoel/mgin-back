076d35af617d77dccff3476207f6fea2d12b3856
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-04 19:00:52 -0300

    Add docstrings to MenuService and OrderService

--- src/services/menu.py
+++ src/services/menu.py
@@ -8,16 +8,34 @@ from ..models.itemrequestfilter import ItemRequestFilterModel
 
 
 class MenuService:
+    """
+    A service class to handle menu-related operations such as fetching categories, items, and applying filters.
+
+    Attributes:
+        db (DBConnection): An instance of the database connection.
+    """
+
     def __init__(self, db: DBConnection):
+        """
+        Initialize the MenuService with a database connection.
+
+        Args:
+            db (DBConnection): An instance of the database connection.
+        """
         self.db = db
 
 
     async def get_categories(self) -> List[CategoryModel]:
         """
-        Get Categories List
-        
+        Retrieve a list of all categories.
+
         Returns:
-            List[CategoryModel]: List of categories
+            List[CategoryModel]: A list of `CategoryModel` instances representing the categories.
+
+        Raises:
+            ValidationError: If there is an issue validating the data from the database.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
         """
         categories: List[CategoryModel] = []
 
@@ -39,13 +57,18 @@ class MenuService:
 
     async def get_category_items(self, category_id: int) -> List[MenuItemModel]:
         """
-        Get Category Items by Category ID
+        Retrieve a list of items belonging to a specific category.
 
         Args:
-            category_id (int): Category ID
+            category_id (int): The ID of the category.
 
         Returns:
-            List[MenuItemModel]: List of items in the category
+            List[MenuItemModel]: A list of `MenuItemModel` instances representing the items in the category.
+
+        Raises:
+            ValidationError: If there is an issue validating the data from the database.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
         """
         items: List[MenuItemModel] = []
 
@@ -55,29 +78,36 @@ class MenuService:
             return items
         
         except ValidationError as e:
-            print(f"Validation Error fetching categories: {e}")
+            print(f"Validation Error fetching category items: {e}")
             raise e
         except PyMongoError as e:
-            print(f"Database Error fetching categories: {e}")
+            print(f"Database Error fetching category items: {e}")
             raise e
         except Exception as e:
-            print(f"Unknown Error fetching categories: {e}")
+            print(f"Unknown Error fetching category items: {e}")
             raise e
         
         
-    async def get_item(self, item_id: int) -> MenuItemModel:
+    async def get_item(self, item_id: int) -> Optional[MenuItemModel]:
         """
-        Get Item by ID
-        
+        Retrieve a specific item by its ID.
+
         Args:
-            item_id (int): Item ID
-        
+            item_id (int): The ID of the item.
+
         Returns:
-            MenuItemModel: Item
+            Optional[MenuItemModel]: An instance of `MenuItemModel` representing the item, or `None` if not found.
+
+        Raises:
+            ValidationError: If there is an issue validating the data from the database.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
         """
         try:
-            item: MenuItemModel = await self.db.get_document("menu_items", item_id)
-            return item
+            doc = await self.db.get_document("menu_items", {"_id": item_id})
+            if doc:
+                return MenuItemModel(**doc)
+            return None
         except ValidationError as e:
             print(f"Validation Error fetching item: {e}")
             raise e
@@ -93,13 +123,18 @@ class MenuService:
         self, filters: Optional[ItemRequestFilterModel] = None
     ) -> List[MenuItemModel]:
         """
-        Get Items with filters
+        Retrieve a list of items filtered by specific criteria.
 
         Args:
-            filters (ItemRequestFilterModel): Search Filters
+            filters (Optional[ItemRequestFilterModel]): An instance of `ItemRequestFilterModel` containing filter criteria. Defaults to `None`.
 
         Returns:
-            List[MenuItemModel]: List of items
+            List[MenuItemModel]: A list of `MenuItemModel` instances representing the filtered items.
+
+        Raises:
+            ValidationError: If there is an issue validating the data from the database.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
         """
         query = filters.as_query() if filters else {}
         
@@ -111,22 +146,26 @@ class MenuService:
             return items
                 
         except ValidationError as e:
-            print(f"Validation Error fetching categories: {e}")
+            print(f"Validation Error fetching filtered items: {e}")
             raise e
         except PyMongoError as e:
-            print(f"Database Error fetching categories: {e}")
+            print(f"Database Error fetching filtered items: {e}")
             raise e
         except Exception as e:
-            print(f"Unknown Error fetching categories: {e}")
+            print(f"Unknown Error fetching filtered items: {e}")
             raise e
                 
     
     async def count_categories(self) -> int:
         """
-        Count Categories
-        
+        Count the total number of categories.
+
         Returns:
-            int: Number of categories
+            int: The total number of categories.
+
+        Raises:
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
         """
         try:
             count = await self.db.count_documents("categories")
@@ -141,10 +180,14 @@ class MenuService:
     
     async def count_items(self) -> int:
         """
-        Count Items
-        
+        Count the total number of items.
+
         Returns:
-            int: Number of items
+            int: The total number of items.
+
+        Raises:
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
         """
         try:
             count = await self.db.count_documents("menu_items")
--- src/services/orders.py
+++ src/services/orders.py
@@ -5,27 +5,46 @@ from ..db import DBConnection
 from ..models.order import OrderModel
 from bson import ObjectId
 
+
 class OrderService:
+    """
+    A service class to handle order-related operations such as creating, retrieving, and managing orders.
+
+    Attributes:
+        db (DBConnection): An instance of the database connection.
+    """
+
     def __init__(self, db: DBConnection):
+        """
+        Initialize the OrderService with a database connection.
+
+        Args:
+            db (DBConnection): An instance of the database connection.
+        """
         self.db = db
         
         
     async def create_order(self, order: OrderModel) -> str:
-        '''
-        Create an Order in the database
-        
+        """
+        Create a new order in the database.
+
         Args:
-            order (OrderModel): Order Object
-        
+            order (OrderModel): An instance of `OrderModel` representing the order to be created.
+
         Returns:
-            str: Order ID
-        '''
-        
+            str: The ID of the newly created order.
+
+        Raises:
+            ValidationError: If there is an issue validating the order data.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
+        """
         try:
-            # Convert dict to OrderModel
+            # Convert dict to OrderModel if necessary
             if isinstance(order, dict):
                 order = OrderModel(**order)
             
+            # Insert the order into the database
             order_id = await self.db.insert_one("orders", order.model_dump())
             return order_id
         
@@ -41,18 +60,22 @@ class OrderService:
         
         
     async def get_orders(self) -> List[OrderModel]:
-        '''
-        Get Orders from the database
-        
+        """
+        Retrieve a list of all orders from the database.
+
         Returns:
-            List[OrderModel]: List of Orders
-        '''
-        
+            List[OrderModel]: A list of `OrderModel` instances representing the orders.
+
+        Raises:
+            ValidationError: If there is an issue validating the order data.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
+        """
         try:
             orders: List[OrderModel] = []
             for doc in await self.db.get_documents_list("orders"):
                 if doc:  
-                    # Convert dict to OrderModel       
+                    # Convert dict to OrderModel if necessary
                     docToStore = OrderModel(**doc) if isinstance(doc, dict) else doc
                     orders.append(docToStore)
             return orders
@@ -69,15 +92,20 @@ class OrderService:
         
         
     async def get_order(self, order_id: str) -> Optional[OrderModel]:
-        '''
-        Get Order by ID
-        
+        """
+        Retrieve a specific order by its ID.
+
         Args:
-            order_id (int): Order ID
-        
+            order_id (str): The ID of the order to retrieve.
+
         Returns:
-            Optional[OrderModel]: Order Object
-        '''
+            Optional[OrderModel]: An instance of `OrderModel` representing the order, or `None` if not found.
+
+        Raises:
+            ValidationError: If there is an issue validating the order data.
+            PyMongoError: If there is a database-related error.
+            Exception: If an unexpected error occurs.
+        """
         try:
             doc = await self.db.get_document("orders", {"_id": ObjectId(order_id)})
             return OrderModel(**doc) if doc else None
