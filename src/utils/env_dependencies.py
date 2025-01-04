07fe065e1880050761185123df0d42c8372dae30
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-04 11:47:20 -0300

    Add function to retrieve settings from environment variables

---
+++ src/utils/env_dependencies.py
@@ -0,0 +1,26 @@
+import os
+
+def get_db_settings() -> dict:
+    '''
+    Get the database settings. If in debug mode, return the default settings. Otherwise, get the settings from the environment variables.
+    
+    Returns:
+        dict: The database settings containing the host, port, database name, username, and password.
+    
+    '''
+    if __debug__:
+        return {
+            'host': 'localhost',
+            'port': 27017,
+            'db_name': 'test_db',
+            'username': 'root',
+            'password': 'root'
+        }
+        
+    return {
+        'host': os.environ.get('MONGO_HOST', 'localhost'),
+        'port': int(os.environ.get('MONGO_PORT', 27017)),
+        'db_name': os.environ.get('MONGO_DB_NAME', None),
+        'username': os.environ.get('MONGO_USERNAME', None),
+        'password': os.environ.get('MONGO_PASSWORD', None)
+    }
