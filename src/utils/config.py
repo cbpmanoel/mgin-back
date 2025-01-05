467d738afd4d01de6148964fc64e25c985792b28
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-05 03:01:27 -0300

    Enhance logging format to include logger name and improve timestamp format

--- src/utils/config.py
+++ src/utils/config.py
@@ -11,7 +11,8 @@ if __debug__:
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
 logging.basicConfig(
     level = LOG_LEVEL,
-    format = "%(asctime)s - %(levelname)s - %(message)s",
+    format="[%(levelname)s] %(asctime)s - [%(name)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)]
 )
 
