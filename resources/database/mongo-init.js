cad97aae0960c08aa058e25fcd1cf171a355efd0
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-05 00:51:57 -0300

    Update mongodb and docker scripts

--- resources/database/mongo-init.js
+++ resources/database/mongo-init.js
@@ -1,23 +1,38 @@
+// Read the JSON file synchronously
 const fs = require('fs');
+let data;
+try {
+    data = JSON.parse(fs.readFileSync('/data/init-data.json', 'utf8'));
+} catch (error) {
+    print('Error reading or parsing the JSON file:', error);
+    quit(1); // Exit with an error code
+}
 
-// Read the JSON file
-console.log('Reading JSON file...');
-const data = JSON.parse(fs.readFileSync('/data/init-data.json', 'utf8'));
-console.log('JSON file read successfully.');
-
-// Connect to MongoDB
-console.log('Connecting to MongoDB...');
+// Connect to the database
 db = db.getSiblingDB('kiosk_db');
-console.log('Connected to MongoDB.');
 
 // Insert categories
-categoriesOnData = data.categories.length;
-console.log(`Inserting ${categoriesOnData} categories into categories collection...`);
-db.categories.insertMany(data.categories);
-console.log('Categories inserted successfully.');
+if (data.categories && data.categories.length > 0) {
+    try {
+        db.categories.insertMany(data.categories);
+        print('Categories inserted successfully.');
+    } catch (error) {
+        print('Error inserting categories:', error);
+    }
+} else {
+    print('No categories to insert.');
+}
 
 // Insert items
-itemsOnData = data.items.length;
-console.log(`Inserting ${itemsOnData} items into menu_items collection...`);
-db.menu_items.insertMany(data.items);
-console.log('Items inserted successfully.');
+if (data.items && data.items.length > 0) {
+    try {
+        db.menu_items.insertMany(data.items);
+        print('Items inserted successfully.');
+    } catch (error) {
+        print('Error inserting items:', error);
+    }
+} else {
+    print('No items to insert.');
+}
+
+print('Database initialization completed.');
